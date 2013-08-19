a18b99db9bf8e15aa50835ad84a988f0b413acc4
pof91 <vincent.euloge@gmail.com>
2013-08-19 06:37:09 +0200

    DistanceVille

--- app/js/controllers/weatherAppCtrl.js
+++ app/js/controllers/weatherAppCtrl.js
@@ -1,12 +1,13 @@
 'use strict';
-var Conditions
+var Conditions,WeatherStations;
 appWeather.controller('weatherAppCtrl', function ($scope,$resource) {
 	//creation de la requete
+	//geolookup/conditions/forecast
 	var methods = {callback: 'JSON_CALLBACK'};
 	var option = {get: {method: 'JSONP'}};
-	var api = $resource('http://api.wunderground.com/api/ea5af3a442586762/geolookup/conditions/forecast/q/France/:location.json', methods, option);
-	var requeteFct = function(location, onSuccess, onError) {
-					return api.get({location: location}, onSuccess, onError);
+	var api = $resource('http://api.wunderground.com/api/ea5af3a442586762/:interet/q/France/:location.json', methods, option);
+	var requeteFct = function(location,interet, onSuccess, onError) {
+					return api.get({location : location,interet : interet}, onSuccess, onError);
 				};
 	$scope.isVisible = false;
 	$scope.greeting = "Hello world ! Please enter a city in the field";
@@ -27,7 +28,38 @@ appWeather.controller('weatherAppCtrl', function ($scope,$resource) {
 		};
 		return requeteFct($scope.city, success);
 	};
+	$scope.updateCity = function(){
+		var success = function(result) {
+			var wS, response;
+			response = result.response;
+			if (response.error) {
+			  $scope.greeting = "Sorry !! " + response.error.description +" : " +  $scope.city;
+			  $scope.isVisible = false;
+			  return $scope.wS = null;
+			} 
+			else {
+				$scope.isVisible = true;
+				$scope.wS = new WeatherStations(result);
+				console.log($scope.wS);
+				return $scope.city = $scope.wS.city;
+			}
+		};
+		return requeteFct($scope.city,'geolookup', success);
+	}
+	
+	$scope.distance = function(lat_a_degre, lon_a_degre, lat_b_degre, lon_b_degre){
+     
+		var R = 6378000 //Rayon de la terre en mètre
+		var lat_a = (Math.PI * lat_a_degre) /180;
+		var lon_a = (Math.PI * lon_a_degre) /180;
+		var lat_b = (Math.PI * lat_b_degre) /180;
+		var lon_b = (Math.PI * lon_b_degre)/180;
+		var d = R * (Math.PI/2 - Math.asin( Math.sin(lat_b) * Math.sin(lat_a) + Math.cos(lon_b - lon_a) * Math.cos(lat_b) * Math.cos(lat_a)))
+		return parseInt(d);
+	}
 });
+ 
+
 
 
 Conditions = function Conditions(apiResponse) {
@@ -44,4 +76,13 @@ Conditions = function Conditions(apiResponse) {
   this.weatherIcon = co.icon_url;
   this.fetchDate = new Date();
   this.foreCastText = fo.txt_forecast.forecastday;
+}
+
+WeatherStations = function WeatherStations(apiResponse) {
+  var apiLoc = apiResponse.location;
+  this.city = apiLoc.city;
+  this.lat = apiLoc.lat;
+  this.lon = apiLoc.lon;
+  this.wSAirports = apiLoc.nearby_weather_stations.airport.station;
+  this.wSPws = apiLoc.nearby_weather_stations.pws.station;
 }
